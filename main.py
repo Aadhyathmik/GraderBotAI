a294c4fbb97c9f921d33dbd08dee4584ad8b7327
Aadhyathmik <52633913+Aadhyathmik@users.noreply.github.com>
2024-10-20 22:38:11 +0000

    Added logos, links and sidebar

--- main.py
+++ main.py
@@ -22,36 +22,81 @@ def pdf_to_text(pdf_file):
 
   return text
 
+def add_space(n=1):
+  for _ in range(n):
+    st.sidebar.text(" ")
 
 def main():
   """
   The main function that handles the Streamlit app logic.
   """
-  # Get OpenAI API key
-  openai_api_key = ""
+
 
 # Title
+
+  st.sidebar.image("Gbot.jpg", use_column_width=True)
+  st.sidebar.divider()
+  lnkd_profile_url="https://www.linkedin.com/in/aadhyathmik/"
+  st.sidebar.markdown("[Aadhyathmik Varahagiri](%s)" % lnkd_profile_url)  
+
+
+  st.sidebar.title("Welcome")
+  st.sidebar.text(" ")
+  st.sidebar.text(" ")
+  st.sidebar.header("This is a free community version of Grader Bot AI")
+  st.sidebar.text(" ")
+  st.sidebar.text(" ")
+  user_input = st.sidebar.text_input("OpenAI API Key")
+  add_space(10)
+
+  # Get OpenAI API key
+  openai_api_key = user_input
+
+  url = "https://www.graderbotai.com"
+  st.sidebar.markdown("Visit this [link](%s) to sign up for Upgraded Version" % url)  
+
+  st.sidebar.text(" ")
+  st.sidebar.text(" ")
+  st.sidebar.text(" ")
+
+  tos="https://graderbotai.com/terms-and-conditions/"
+  pp="https://graderbotai.com/privacy-policy/"
+  st.sidebar.text(" ")
+  st.sidebar.text(" ")
+  st.sidebar.markdown("[Terms of Service](%s)" % tos)  
+  st.sidebar.text(" ")
+  st.sidebar.text(" ")
+  st.sidebar.markdown("[Privacy Policy](%s)" % pp)  
+  
+
+  image_file="Gbot.jpg"
+  st.image(image_file, use_column_width=True)
+  link_url="https://graderbotai.com/"
+  
+
+
+  st.divider()
   st.title("Assignment Evaluation and Feedback")
 
   # Upload files (Syllabus, Rubric, Papers)
   uploaded_syllabus = st.file_uploader("Upload syllabus", type=("txt", "md", "pdf"))
   uploaded_rubric = st.file_uploader("Upload rubric", type=("txt", "md", "pdf"))
   uploaded_papers = st.file_uploader("Upload papers", type=("txt", "md", "pdf"), accept_multiple_files=False)
-  question = st.text_input(
-      "Ask something about the article(s)",
-      placeholder="Can you give me a short summary?",
-      disabled=not uploaded_rubric,
-  )
+  #question = st.text_input(
+  #    "Ask something about the article(s)",
+  #    placeholder="Can you give me a short summary?",
+  #    disabled=not uploaded_rubric,
+  #)
 
   # Check for missing inputs
   if (uploaded_syllabus
       and uploaded_rubric
       and uploaded_papers
-      and question
+      #and question
       and not openai_api_key
   ):
       st.info("1 or more inputs are missing. Please provide the missing input(s) to continue.")
-  elif uploaded_syllabus and uploaded_rubric and uploaded_papers and question and openai_api_key:
+  elif uploaded_syllabus and uploaded_rubric and uploaded_papers and openai_api_key:
       
       # Process files
       syllabus_text = get_text_from_file(uploaded_syllabus)
@@ -63,7 +108,7 @@ def main():
       all_responses = []
         
         # Given prompt
-      prompt = f"""Here's a syllabus:\n\n{syllabus_text}\n\nHere's a rubric:\n\n{rubric_text}\n\nHere's a paper:\n\n{paper_text}\n\n{question}"""
+      prompt = f"""Here's a syllabus:\n\n{syllabus_text}\n\nHere's a rubric:\n\n{rubric_text}\n\nHere's a paper:\n\n{paper_text}"""
 
         # Model
       client = openai.Client(api_key=openai_api_key)
@@ -71,7 +116,7 @@ def main():
           model="gpt-3.5-turbo",
           messages=[
               {"role": "user", "content": prompt},
-              {"role": "system", "content": "Give a clear and coherent answer to the given question"},
+              {"role": "system", "content": "Grade the answers using syllabus, rubric and uploaded response"},
           ],
           temperature=1,
           max_tokens=1000,
